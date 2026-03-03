2893a42abc1eef121be41761978ca3a9914e4525
Manas Pradhan <62531748+pradhan567@users.noreply.github.com>
2026-03-03 10:26:01 +0530

    Update library configuration syntax in folder.groovy

--- jobs-dsl/production/folder.groovy
+++ jobs-dsl/production/folder.groovy
@@ -3,17 +3,22 @@ folder('demo-folder') {
         folderLibraries {
             libraries {
                 libraryConfiguration {
-                    name("demo-lib")
-                    defaultVersion("main")
+                    name('demo-lib')
+                    defaultVersion('main')
                     implicit(false)
                     allowVersionOverride(true)
 
                     retriever {
                         modernSCM {
-                            scm {
-                                git {
-                                    remote {
-                                        url("https://github.com/jenkinsci/job-dsl-plugin.git")
+                            scm(class: 'hudson.plugins.git.GitSCM') {
+                                userRemoteConfigs {
+                                    userRemoteConfig {
+                                        url('https://github.com/jenkinsci/job-dsl-plugin.git')
+                                    }
+                                }
+                                branches {
+                                    branchSpec {
+                                        name('*/main')
                                     }
                                 }
                             }
