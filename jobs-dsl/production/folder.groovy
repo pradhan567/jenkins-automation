b2f8e4738544b7e5fdf0f2ffec6986de9d653b20
Manas Pradhan <62531748+pradhan567@users.noreply.github.com>
2026-03-03 10:31:45 +0530

    Simplify folder configuration and add description
    Removed folder properties and added a description.

--- jobs-dsl/production/folder.groovy
+++ jobs-dsl/production/folder.groovy
@@ -1,27 +1,3 @@
 folder('demo-folder') {
-    properties {
-        folderLibraries {
-            libraries {
-                libraryConfiguration {
-                    name('demo-lib')
-                    defaultVersion('main')
-                    implicit(false)
-                    allowVersionOverride(true)
-
-                    retriever {
-                        legacySCM {
-                            scm {
-                                git {
-                                    remote {
-                                        url('https://github.com/jenkinsci/job-dsl-plugin.git')
-                                    }
-                                    branch('*/main')
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+    description('Initial version -2 ')
 }
